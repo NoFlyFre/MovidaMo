5b8a90827272774c47bda10284fe3f1afcce9914
NoFlyFre <93214917+NoFlyFre@users.noreply.github.com>
2023-07-08 13:13:00 +0200

    change allowed hosts

--- movidamo/settings.py
+++ movidamo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t88$g&ymtn5#6kc-o*kqkqzol($bl2u8@b--5iu+adc2kv^p^7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','192.168.1.161']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
