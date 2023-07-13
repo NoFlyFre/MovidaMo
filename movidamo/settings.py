3fd08f1a915e65e5b614c8b646466473c215ac6a
NoFlyFre <93214917+NoFlyFre@users.noreply.github.com>
2023-07-13 22:47:09 +0200

    Update settings.py

--- movidamo/settings.py
+++ movidamo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t88$g&ymtn5#6kc-o*kqkqzol($bl2u8@b--5iu+adc2kv^p^7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['movidamo.azurewebsites.net']
 
